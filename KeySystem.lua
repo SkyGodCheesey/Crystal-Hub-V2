268536da43ffaaa2e2742a9d44ba269837159003
SkyGodCheesey <94723811+SkyGodCheesey@users.noreply.github.com>
2022-12-05 18:19:03 -0500

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -1,2 +1,13 @@
---Coming Soon!
-:)
+--Crystal Scripts Key Gui
+	local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+	local Window = Rayfield:CreateWindow({
+		KeySystem = true,
+		KeySettings = {
+		Title = "Crystal Key System",
+		Subtitle = "",
+		Note = "Join the discord (discord.gg/3r49xTrcbz)",
+		SaveKey = false,
+		GrabKeyFromSite = true,
+		Key = "https://pastebin.com/raw/BdQjWf6C"
+		}
+	})
